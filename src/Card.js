51680dc5174b2c849e59ca316aec9bd512598691
William Serna <wsernalaverde@gmail.com>
2017-08-03 19:20:57 -0500

    actualización de card con semantic ui

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
 
-class Card extends Component{
+class Cards extends Component{
  render(){
      const {items} = this.props;//Con destructuring sacamos el items que le mandamos en el objeto de propiedades
-     
-      return(
-          <div>
-          {items.map((item) => {
-            return(
-            <div className="contCard">
+     /* <div className="contCard">
                 <div className="cont-img">
                     <img src={item.inputImage} alt=""/>
                 </div>
                 <div className="cont-excerpt">
                     <span>{item.inputExcerpt}</span>
                 </div>
-            </div>
+            </div> */
+      return(
+          <div>
+          {items.map((item) => {
+            return(
+            <Card>
+                <Image src={item.inputImage} />
+                <Card.Content>
+                <Card.Header>
+                   {item.inputExcerpt}
+                </Card.Header>
+                <Card.Meta>
+                    <span className='date'>
+                    Joined in 2015
+                    </span>
+                </Card.Meta>
+                <Card.Description>
+                    Matthew is a musician living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                <a>
+                    <Icon name='user' />
+                    22 Friends
+                </a>
+                </Card.Content>
+            </Card> 
+           
             );
          })
         }</div>
@@ -24,4 +47,4 @@ class Card extends Component{
  }
 }
 
-export default Card;
+export default Cards;
